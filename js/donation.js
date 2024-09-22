caaaeefe84a4dc96d30661f4a6cb95f949ec34e0
fnhnafiz <nafsinsifan2k19@gmail.com>
2024-09-23 04:31:47 +0600

    update total donation ammount with js

--- js/donation.js
+++ js/donation.js
@@ -6,9 +6,15 @@ noakhaliDonateBtn.addEventListener("click", function () {
   );
 
   if (noakhaliInputField > 0 && !isNaN(noakhaliInputField)) {
+    const totalDonationAmount = parseFloat(
+      document.getElementById("total-donation-available-Ammount").innerText
+    );
     const noakhaliAvailableAmount = parseFloat(
       document.getElementById("noakhali-donation-available-amount").innerText
     );
+    const totalAmount = totalDonationAmount - noakhaliInputField;
+    document.getElementById("total-donation-available-Ammount").innerText =
+      totalAmount;
     // console.log(noakhaliAvailableAmount);
     const totalNoakhaliDonationAmount =
       noakhaliAvailableAmount + noakhaliInputField;
